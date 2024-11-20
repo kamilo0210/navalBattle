88aaf81802a27610ba9255fb733b4ee65925e286
Cristian <pinzon.cristian@correounivalle.edu.co>
2024-11-19 23:39:15 -0500

    Configuración de acciones

--- src/main/java/com/example/navalbattle/model/SafeShot.java
+++ src/main/java/com/example/navalbattle/model/SafeShot.java
@@ -1,4 +1,39 @@
 package com.example.navalbattle.model;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class SafeShot {
+
+    private ImageView imgWater;
+    private double posImgX, posImgY;
+
+    public SafeShot(){
+        imgWater = new ImageView(new Image(String.valueOf(getClass().getResource("/com/example/navalbattle/images/Agua.png"))));
+        imgWater.setFitHeight(32);
+        imgWater.setFitWidth(32);
+        // No configurar LayoutX y LayoutY aquí, ya que se configurarán después.
+    }
+
+    public ImageView getImageView(){
+        return imgWater;
+    }
+
+    public double getPosImgX(){
+        return posImgX;
+    }
+
+    public double getPosImgY(){
+        return posImgY;
+    }
+
+    public void setPosImgX(double posImgX){
+        this.posImgX = posImgX;
+        imgWater.setLayoutX(posImgX);
+    }
+
+    public void setPosImgY(double posImgY){
+        this.posImgY = posImgY;
+        imgWater.setLayoutY(posImgY);
+    }
 }
